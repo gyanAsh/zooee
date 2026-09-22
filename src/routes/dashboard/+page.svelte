7d65d4fb710b662758c4741704750ef7af0a1144
gyanAsh <gyanashekka1738@gmail.com>
2026-09-22 14:49:31 +0530

    chore :  bits-ui libary added

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import Tooltip from '$lib/components/ui/tooltip.svelte';
 </script>
 
 <!-- If the user goes to ://domain.com, this will output "hello" -->
 <h1>Thsi jsut dashbaord</h1>
+<Tooltip />
